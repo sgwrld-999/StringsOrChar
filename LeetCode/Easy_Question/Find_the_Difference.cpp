2d1ab8749117f21834218b9fa8dab7faed336ba3
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-19 23:40:39 +0530

    Added the one more way  to add to solve the question

--- LeetCode/Easy_Question/Find_the_Difference.cpp
+++ LeetCode/Easy_Question/Find_the_Difference.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
+    char findTheDifferenceBysorting(string s, string t) {
+        //TC : O(logN * N)
         sort(begin(s), end(s));
         sort(begin(t), end(t));
         for (int i = 0; i < t.size(); i++) {
@@ -13,6 +14,22 @@ public:
                 }
             } else {
                 return t[i];
+            }
+        }
+        return '\0';
+    }
+    char findTheDifferenceUsingMap(string s, string t) {
+        //TC : Linear
+        unordered_map<char, int> mp;
+        for (auto c : t) {
+            mp[c]++;
+        }
+        for (auto c : s) {
+            mp[c]--;
+        }
+        for (auto c : mp) {
+            if (c.second == 1) {
+                return c.first;
             }
         }
         return '\0';
@@ -32,7 +49,7 @@ int main(int argc, char const *argv[]) {
     Solution solution;
 
     // Call the findTheDifference function and display the result
-    char result = solution.findTheDifference(s, t);
+    char result = solution.findTheDifferenceUsingMap(s, t);
     cout << "The extra character is: " << result << endl;
 
     return 0;
